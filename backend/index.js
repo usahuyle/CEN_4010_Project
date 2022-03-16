f4c73dbf54d0c830cb4aa3c46c138d0b81b3873d
spartanDavid22 <90160219+spartanDavid22@users.noreply.github.com>
2022-03-16 14:53:56 -0400

    Set up connection to AWS RDS on the backend
    I set up the connection to the database I created in AWS
    -To query the database to get information simply type connection.query(query, data, callback)

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,23 @@
 const express = require("express");
 const app = express();
 
+//Setting up connection to AWS RDS database
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host     : "database-1.cjwufjoqjxuz.us-east-1.rds.amazonaws.com",
+    user     : "admin",
+    password : "westboca2022",
+    port     : "3307",
+    database : "westboca"
+})
+
+
+
+
+//Closing connection to database
+connection.end();
+
 //Setting up the backend to listen on a specific port
 app.listen(3000, ()=>{
     console.log("Server listening on port 3000...");
